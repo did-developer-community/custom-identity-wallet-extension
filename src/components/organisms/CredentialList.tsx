933be5c933b12084a6f4aaad2625884725f64ad0
masaki obayashi <40846038+sinsinpurin@users.noreply.github.com>
2022-09-21 14:53:35 +0900

    chore: If vc is empty,showing "You don't have Credential"

--- src/components/organisms/CredentialList.tsx
+++ src/components/organisms/CredentialList.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid } from "@chakra-ui/react";
+import { Box, Center, Grid, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import React from "react";
 
@@ -10,22 +10,34 @@ export const CredentialList: React.FC = () => {
   const router = useRouter();
   const { storedVCs } = useStoredVCs();
   return (
-    <Grid gap={4}>
-      {storedVCs.map((storedVC) => {
-        return (
-          <Box key={storedVC.id}>
-            <Box
-              cursor={"pointer"}
-              _hover={{ opacity: 0.9, transition: "0.2s" }}
-              onClick={() => {
-                router.push(CREDENTIAL_DETAIL_PAGE_PATH + "?vcID=" + storedVC.id);
-              }}
-            >
-              <CredentialCard storedVC={storedVC} />
-            </Box>
-          </Box>
-        );
-      })}
-    </Grid>
+    <>
+      <Grid gap={4}>
+        {storedVCs.length !== 0 ? (
+          storedVCs.map((storedVC) => {
+            return (
+              <Box key={storedVC.id} overflowY={"scroll"}>
+                <Box
+                  cursor={"pointer"}
+                  _hover={{ opacity: 0.9, transition: "0.2s" }}
+                  onClick={() => {
+                    router.push(CREDENTIAL_DETAIL_PAGE_PATH + "?vcID=" + storedVC.id);
+                  }}
+                >
+                  <CredentialCard storedVC={storedVC} />
+                </Box>
+              </Box>
+            );
+          })
+        ) : (
+          <>
+            <Center h="100%">
+              <Text fontWeight={"bold"} color={"gray.400"} fontSize={"large"}>
+                You don&apos;t have Credential
+              </Text>
+            </Center>
+          </>
+        )}
+      </Grid>
+    </>
   );
 };
